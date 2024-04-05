9019e2144300d262cb26672da35a08e548b4d364
morten.lund@maskon.no <morten.lund@maskon.no>
2024-04-05 13:40:19 +0200

    Initial

---
+++ lib/ex_webp.ex
@@ -0,0 +1,18 @@
+defmodule ExWebp do
+  @moduledoc """
+  Documentation for `ExWebp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExWebp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_webp_test.exs
@@ -0,0 +1,8 @@
+defmodule ExWebpTest do
+  use ExUnit.Case
+  doctest ExWebp
+
+  test "greets the world" do
+    assert ExWebp.hello() == :world
+  end
+end
