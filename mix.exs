2d2359810799d45e0058bc41f9d7953377c7c722
morten.lund@maskon.no <morten.lund@maskon.no>
2026-02-12 09:54:13 +0100

    Update version

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule ExWebp.MixProject do
   defp deps do
     [
       {:rustler_precompiled, "~> 0.8"},
-      {:rustler, "~> 0.36.2", optional: true}
+      {:rustler, "~> 0.37", optional: true}
     ]
   end
 end
