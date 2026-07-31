f9415e08a4ec7b152203bc51e78c684b58018290
morten.lund@maskon.no <morten.lund@maskon.no>
2026-07-31 13:22:14 +0200

    Update checksum

--- checksum-Elixir.ExWebp.exs
+++ checksum-Elixir.ExWebp.exs
@@ -1,10 +1,10 @@
 %{
-  "ex_webp-v0.1.8-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:4ae5affaf784744edc23b9cee8a7e56286db6d6c622ea90e1bfe5a7a430e0096",
-  "ex_webp-v0.1.8-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:8b013d676b3e0d3df4c110a3097c87cdd4fcc03b93817b540a6025d373a4dfd4",
-  "libex_webp-v0.1.8-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:824018544eaa974cdf0d3fab224420342575fd22803d181366724ae14919b74a",
-  "libex_webp-v0.1.8-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:2d6c9a3d3660824cab7c76c7db6ecf7e0925734f9db655f354da3a71da050f6e",
-  "libex_webp-v0.1.8-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:732eedc0e27fb4939e9a4ecd709460ab6e38c7213dacd7373a066c0ac2a62bfe",
-  "libex_webp-v0.1.8-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:422f9f6bfc5faadab7b7cb2b1292fa16290a2083593e8a26a10fd28578e6ccff",
-  "libex_webp-v0.1.8-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:447315069daa557258399608ec9da66b758199d49fada0b651426ccf634b2278",
-  "libex_webp-v0.1.8-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:94ae5e5275b823a1a93e1cba291ae2d18ea5b1c591ff149f3669cbf636137a7e",
+  "ex_webp-v0.1.9-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:b957a2f0a4d42535577d70238def6df34269fc8e998466ce70e576d81f19e6be",
+  "ex_webp-v0.1.9-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:ca89629d51c9378717c440622bf26e461172d1e34688f948aff311965c01c422",
+  "libex_webp-v0.1.9-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:a3ad25f23f5de9ad9c8b5678784dfd3338c5af86e41c9614160cd5dda6f78ed0",
+  "libex_webp-v0.1.9-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:32d5981da35aa2413c6943d0e1ab9e6b48f0b00e496c30f80887ee054c78f525",
+  "libex_webp-v0.1.9-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:e4ecd9ef472aa72c7161a2d73cce43c632a757bba0d89572279299fdfd85882b",
+  "libex_webp-v0.1.9-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:eb7a210c6aff0b4775192807dec9c1325d43d9f21a781a4b3162bf8f24a1c6eb",
+  "libex_webp-v0.1.9-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4695bc76ac700dcf8e0a2a500e973ea823d807fe3176bc6060d1008dbc25a99f",
+  "libex_webp-v0.1.9-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:6f3c57dea0b955ba09725db6a2ba1b337d7bf60ba31f7f0db280fcf19104a017",
 }
